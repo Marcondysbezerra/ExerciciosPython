ffa80564bfbdabc3300638b1f52a6fe1a3912ec3
Marcondys Santos <marcondysbezerra@gmail.com>
2023-03-06 21:11:14 -0300

    Quinto commit

--- exercicio15.py
+++ exercicio15.py
@@ -15,3 +15,23 @@ Calcule e mostre o total do seu salário no referido mês, sabendo-se que são d
     - Sindicato ( 5%) : R$
     = Salário Liquido : R$
 """
+
+reais_por_hora  = float(input("Digite quanto você ganha por hora: "))
+horas_trabalhadas = int(input("Quantas horas você trabalhou nesse mês? "))
+
+salario_bruto = reais_por_hora * horas_trabalhadas
+
+imposto_de_renda = salario_bruto * 0.11
+
+inss = salario_bruto * 0.08
+
+sindicato = salario_bruto * 0.05
+
+descontos = [imposto_de_renda, inss, sindicato]
+salario_liquido = salario_bruto - sum(descontos)
+
+print("+ Salário Bruto R${:.2f}".format(salario_bruto))
+print("- Impsto de Renda R${:.2f}".format(imposto_de_renda))
+print("- INSS R${:.2f}".format(inss))
+print("- Sindicato R${:.2f}".format(sindicato))
+print("= salário Liquido R${:.2f}".format(salario_liquido))
